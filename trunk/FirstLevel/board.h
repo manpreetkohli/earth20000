f0defd1466006275c9c338c0b1ac4582a11df132
msdelhi88 <msdelhi88@5b84c28e-98d8-11de-b8e1-0bcae535652c>
2009-10-03 13:37:57 +0000

    header file for the board for the first level

---
+++ trunk/FirstLevel/board.h
@@ -0,0 +1,23 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+#include <QGraphicsView>
+#include <QGraphicsScene>
+
+
+class Board
+{
+
+    public:
+        QGraphicsScene *scene;
+        QGraphicsView *view;
+
+        QTimer *timer;
+     //   QPalette *palette;
+      //  QPixmap *bgImage;
+
+        Board();
+        void showBoard();
+
+};
+#endif // BOARD_H
