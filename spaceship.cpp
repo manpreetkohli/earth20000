6cd094f4fd68a621214e79969428c998b0f8c71a
ivan.collazo1 <ivan.collazo1@5b84c28e-98d8-11de-b8e1-0bcae535652c>
2009-10-11 01:59:21 +0000

    updated ship

--- spaceship.cpp
+++ spaceship.cpp
@@ -6,42 +6,50 @@
 #include <QGraphicsScene>
 #include <QStyleOption>
 #include <QGraphicsItem>
-#include <iostream>
+//#include <iostream>
+#include <QDebug>
 
 //MoveBehavior shipMoving;
 //ShotBehavior shipShooting;
 
-// constructor
-spaceShip::spaceShip()
-        : width(100), height(60), left(325), top(620), color(Qt::red)
-{
+//using namespace std;
 
+// constructor
+SpaceShip::SpaceShip()
+{
+    width = 100;
+    height = 60;
+    left = 325;
+    top = 620;
+    color = (Qt::red);
+
+    setPos(left, top);
 }
 
 // destructor
-spaceShip::~spaceShip()
-{
+//SpaceShip::~SpaceShip()
+//{
 
-}
+//}
 
 // the bounding rectangle of the object for collision detection
-QRectF spaceShip::boundingRect()
+QRectF SpaceShip::boundingRect() const
 {
     return QRectF(left, top, width, height);
 }
 
 // called whenever the spaceShip needs to be drawn
-void spaceShip::paint (QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+void SpaceShip::paint (QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
 {
         // For the fun of it, I am going to make the character turn
         // Red when it detects it has collided with something else.
         // listOfCollidingItems returns a QList of items that you
         // can later traverse to examine what collided with the character.
-        QList<QGraphicsItem*> listOfCollidingItems = collidingItems();
+      //  QList<QGraphicsItem*> listOfCollidingItems = collidingItems();
 
         // If nothing collided then draw the character in black, otherwise red.
-        if (listOfCollidingItems.isEmpty()) painter->setBrush(Qt::black);
-        else painter->setBrush(Qt::red);
+      //  if (listOfCollidingItems.isEmpty()) painter->setBrush(Qt::black);
+      //  else painter->setBrush(Qt::red);
 
 
     painter->setBrush(color);
@@ -49,13 +57,13 @@ void spaceShip::paint (QPainter *painter, const QStyleOptionGraphicsItem *option
 }
 
 
-//void spaceShip::performShipMovement()
+//void SpaceShip::performShipMovement()
 //{
 //    shipMoving.move();
 
 //}
 
-//void spaceShip::performShootingMovement()
+//void SpaceShip::performShootingMovement()
 //{
 //    shipShooting.shot();
 //}
