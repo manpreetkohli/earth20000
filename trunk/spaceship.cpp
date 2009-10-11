f549f5da5b22a3eb8fc9ad66652c12fc13595af4
ivan.collazo1 <ivan.collazo1@5b84c28e-98d8-11de-b8e1-0bcae535652c>
2009-10-11 04:39:29 +0000

    ship updated and place it on the board

--- trunk/spaceship.cpp
+++ trunk/spaceship.cpp
@@ -3,39 +3,36 @@
 //#include "ShotBehavior.h"
 
 #include <QPainter>
-#include <QGraphicsScene>
+//#include <QGraphicsScene>
 #include <QStyleOption>
 #include <QGraphicsItem>
-//#include <iostream>
+#include <iostream>
 #include <QDebug>
 
 //MoveBehavior shipMoving;
 //ShotBehavior shipShooting;
 
-//using namespace std;
+using namespace std;
 
 // constructor
 SpaceShip::SpaceShip()
 {
-    width = 100;
-    height = 60;
-    left = 325;
-    top = 620;
+    width = 0; //100
+    height = 0; //60
+    left = 0;  //325
+    top = 0;   //620
     color = (Qt::red);
-
+    qDebug() << "Constructor" ;
     setPos(left, top);
+
+   // setFlag(QGraphicsItem::boundingRect());
+
 }
 
 // destructor
-//SpaceShip::~SpaceShip()
-//{
-
-//}
-
-// the bounding rectangle of the object for collision detection
-QRectF SpaceShip::boundingRect() const
+SpaceShip::~SpaceShip()
 {
-    return QRectF(left, top, width, height);
+    qDebug() << "Destructor" ;
 }
 
 // called whenever the spaceShip needs to be drawn
@@ -45,15 +42,19 @@ void SpaceShip::paint (QPainter *painter, const QStyleOptionGraphicsItem *option
         // Red when it detects it has collided with something else.
         // listOfCollidingItems returns a QList of items that you
         // can later traverse to examine what collided with the character.
-      //  QList<QGraphicsItem*> listOfCollidingItems = collidingItems();
+        QList<QGraphicsItem*> listOfCollidingItems = collidingItems();
 
         // If nothing collided then draw the character in black, otherwise red.
-      //  if (listOfCollidingItems.isEmpty()) painter->setBrush(Qt::black);
-      //  else painter->setBrush(Qt::red);
-
-
+        if (listOfCollidingItems.isEmpty()) painter->setBrush(Qt::black);
+        else painter->setBrush(Qt::blue);
     painter->setBrush(color);
-    painter->drawRect(left, top, width, height);
+    painter->drawRect(325, 640, 80, 30);
+}
+
+// the bounding rectangle of the object for collision detection
+QRectF SpaceShip::boundingRect() const
+{
+    return QRectF(left, top, width, height);
 }
 
 
