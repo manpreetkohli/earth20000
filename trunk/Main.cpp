a72dfd7bad103cd5aab64054cf8a0cb88f8d0e78
msdelhi88 <msdelhi88@5b84c28e-98d8-11de-b8e1-0bcae535652c>
2009-10-15 04:23:03 +0000

    Comments added

--- trunk/Main.cpp
+++ trunk/Main.cpp
@@ -1,17 +1,26 @@
-#include "form.h"
+/**
+ * Manpreet Kohli
+ * CS 340, Fall 2009
+ *
+ * Main.cpp: creates an app and an object of type Form
+ *
+ */
 
+
+// necessary include files
+#include "form.h"
 #include <QApplication>
 
+// main()
 int main(int argc, char **argv)
 {
-
     QApplication app(argc, argv);
-    Form form;
+    Form form;      // create an object of type Form
 
+    // set the dimensions of the graphics window
     form.setGeometry(250, 40, 800, 725);
-    form.show();
 
+    form.show();    // now actually display the window
 
     return app.exec();
-
 }
