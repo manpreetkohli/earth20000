cd40d2485b39f2cfb96a01760937112e4ac72acf
msdelhi88 <msdelhi88@5b84c28e-98d8-11de-b8e1-0bcae535652c>
2009-10-15 04:14:43 +0000

    Ball class nicely commented

--- trunk/ball.h
+++ trunk/ball.h
@@ -1,31 +1,35 @@
+/**
+ * Manpreet Kohli
+ * CS 340, Fall 2009
+ *
+ * Ball.h: header file for the Ball class that creates the ball that bounces around inside the level
+ *
+ */
+
 #ifndef BALL_H
 #define BALL_H
 
-
+// include necessary files
+#include <QPainter>
 #include <QGraphicsItem>
+#include <iostream>
+#include "levelOne.h"
+#include "levelTwo.h"
+
 
 class Ball : public QGraphicsItem
 {
-
     private:
-
-
         QPixmap ballImage;
-        qreal bounceBoundsX, bounceBoundsY, directionX, directionY, positionX, positionY;
+        qreal directionX, directionY, positionX, positionY;
         qreal viewWidth, viewHeight, width, height;
-
 
     public:
         Ball();
-
         void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
-
         QRectF boundingRect() const;
-
         void setBounceBounds(qreal x, qreal y);
-
         void Ball::advance(int phase);
-
 };
 
 #endif // BALL_H
