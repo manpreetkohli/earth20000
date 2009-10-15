ea97481bbcfdcf99213fb027c88f9928a98b0efb
msdelhi88 <msdelhi88@5b84c28e-98d8-11de-b8e1-0bcae535652c>
2009-10-15 04:17:43 +0000

    Board class nicely commented

--- board.h
+++ board.h
@@ -1,23 +1,32 @@
+/**
+ * Manpreet Kohli
+ * CS 340, Fall 2009
+ *
+ * board.h: header file for the Board class that sets up the board for the game- adds a scene to a view,
+ * and then adds the various QGraphicsItems to the board
+ *
+ */
+
+
 #ifndef BOARD_H
 #define BOARD_H
 
+// include necessary files
+#include "block.h"
+#include "levelOne.h"
+#include "levelTwo.h"
+#include "levelFive.h"
+#include <QTimer>
 #include <QGraphicsView>
 #include <QGraphicsScene>
 
-
 class Board
 {
-
     public:
         QGraphicsScene *scene;
-    //    QGraphicsView *view;
-
         QTimer *timer;
-     //   QPalette *palette;
-      //  QPixmap *bgImage;
-
         Board(QGraphicsView *view);
-        void showBoard();
+        void connectTimerToBall();
 
 };
 #endif // BOARD_H
