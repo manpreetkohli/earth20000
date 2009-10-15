e50b4b66c1cafc077d02fe4f43631f597010ebb3
msdelhi88 <msdelhi88@5b84c28e-98d8-11de-b8e1-0bcae535652c>
2009-10-15 05:09:21 +0000

    Ball movement updated so that the ball re-spawns in it's initial location once it goes beyond the bottom part (-Y axis) of the screen

--- ball.cpp
+++ ball.cpp
@@ -46,8 +46,18 @@ void Ball::advance(int phase)
 
         // comment out the positionY >= 85 part to make it not
         // bounce back on hitting the bottom part of the screen
-        if ((positionY >= 85) || (positionY <= -630))
+        if (positionY <= -630)
             directionY = -directionY;
+
+        // if the ball went beyond the bottom of the screen
+        if (positionY >= 85)
+        {
+            positionX = 0;                          // reset X coordinate to 0
+            positionY = 0;                          // reset Y coordinate to 0
+            setPos(positionX, positionY);           // set the coordinates to initial position
+            directionX = 1;                         // set the X-axis increment for the movement
+            directionY = -1;                        // set the Y-axis increment for the movement
+        }
 
         // set the new position of the ball
         setPos(positionX,positionY);
