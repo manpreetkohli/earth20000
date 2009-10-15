d1975aef6744d143d2b927a9a45b555f513b4901
msdelhi88 <msdelhi88@5b84c28e-98d8-11de-b8e1-0bcae535652c>
2009-10-15 04:17:43 +0000

    Board class nicely commented

--- trunk/board.cpp
+++ trunk/board.cpp
@@ -1,37 +1,41 @@
+/**
+ * Manpreet Kohli
+ * CS 340, Fall 2009
+ *
+ * board.cpp: sets up the board for the game- adds a scene to a view, and then adds the various
+ * QGraphicsItems to the board
+ *
+ */
+
+// include the header file for this class
 #include "board.h"
-#include "block.h"
-#include "levelOne.h"
-#include "levelTwo.h"
-#include "levelFive.h"
 
-#include <QTimer>
-
+// constructor
 Board::Board(QGraphicsView *view)
 {
-    scene = new QGraphicsScene();
-    int width = view->geometry().width()-5;
-    int height = view->geometry().height()-5;
-    scene->setSceneRect(0, 0, width, height);
+    scene = new QGraphicsScene();           // create a new scene
+    int width = view->geometry().width() - 5;
+    int height = view->geometry().height() - 5;
+    scene->setSceneRect(0, 0, width, height);       // set dimensions of the scene
 
-    //levelOne *theFirstLevel = new levelOne(scene);
-    //levelTwo *theSecondLevel = new levelTwo(scene);
-    levelFive *theFifthLevel = new levelFive(scene);
+    levelOne *theFirstLevel = new levelOne(scene);
+    // levelTwo *theSecondLevel = new levelTwo(scene);
+    // levelFive *theFifthLevel = new levelFive(scene);
 
     view->setRenderHint(QPainter::Antialiasing);
     view->setCacheMode(QGraphicsView::CacheBackground);
-    view->setScene(scene);
-
-    timer = new QTimer();
+    view->setScene(scene);          // set the created scene inside the view
+    timer = new QTimer();       // create a new QTimer() instance
 }
 
-void Board::showBoard()
+// method to connect the timer to the ball inside the board
+void Board::connectTimerToBall()
 {
-
-     QObject::connect(timer, SIGNAL(timeout()), scene, SLOT(advance()));
+    // connect the timer to the advance method inside the Ball class
+    QObject::connect(timer, SIGNAL(timeout()), scene, SLOT(advance()));
 
      // Set the timer to trigger every 3 ms.
-    timer->start(1000 / 170);
-
+    timer->start(3);
 }
 
 
